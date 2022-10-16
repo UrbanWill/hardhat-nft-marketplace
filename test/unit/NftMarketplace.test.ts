faf5b9b96bbfa2d4a117e47688b209916d731343
UrbanWill <willian.urban@gmail.com>
2022-10-16 15:20:59 +0700

    added tests for listing item

--- test/unit/NftMarketplace.test.ts
+++ test/unit/NftMarketplace.test.ts
@@ -5,14 +5,19 @@ const { network, deployments, ethers } = require("hardhat");
 
 !developmentChains.includes(network.name)
   ? describe.skip
-  : describe("Nft Marketplace Unit Tests", function () {
+  : describe("Nft Marketplace Unit Tests", () => {
       let nftMarketplace, nftMarketplaceContract, basicNft, basicNftContract;
       const PRICE = ethers.utils.parseEther("0.1");
       const TOKEN_ID = 0;
+      let accounts: SignerWithAddress;
+      let deployer: SignerWithAddress;
+      let user: SignerWithAddress;
 
       beforeEach(async () => {
-        const accounts: SignerWithAddress[] = await ethers.getSigners();
-        const [deployer, user] = accounts;
+        accounts = await ethers.getSigners();
+        deployer = accounts[0];
+        user = accounts[1];
+
         await deployments.fixture(["all"]);
         nftMarketplaceContract = await ethers.getContract("NftMarketplace");
         nftMarketplace = nftMarketplaceContract.connect(deployer);
@@ -23,10 +28,36 @@ const { network, deployments, ethers } = require("hardhat");
       });
 
       describe("listItem", function () {
-        it("emits an event after listing an item", async function () {
+        it("emits an event after listing an item", async () => {
           expect(await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)).to.emit(
             "ItemListed",
           );
+        });
+        it("exclusively items that haven't been listed", async () => {
+          await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE);
+          const error = `AlreadyListed("${basicNft.address}", ${TOKEN_ID})`;
+          await expect(
+            nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE),
+          ).to.be.revertedWith(error);
+        });
+        it("exclusively allows owners to list", async () => {
+          nftMarketplace = nftMarketplaceContract.connect(user);
+          await basicNft.approve(user.address, TOKEN_ID);
+          await expect(
+            nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE),
+          ).to.be.revertedWith("NotOwner");
+        });
+        it("needs approvals to list item", async () => {
+          await basicNft.approve(ethers.constants.AddressZero, TOKEN_ID);
+          await expect(
+            nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE),
+          ).to.be.revertedWith("NotApprovedForMarketplace");
+        });
+        it("Updates listing with seller and price", async () => {
+          await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE);
+          const listing = await nftMarketplace.getListing(basicNft.address, TOKEN_ID);
+          assert(listing.price.toString() == PRICE.toString());
+          assert(listing.seller.toString() == deployer.address);
         });
       });
     });
