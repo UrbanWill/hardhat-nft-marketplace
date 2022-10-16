389998f0886319fa4dab6490ea4588df2cafc7fc
UrbanWill <willian.urban@gmail.com>
2022-10-16 17:33:10 +0700

    created mint and list item script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # hardhat-nft-marketplace
+
+## How to run scripts
+1- Open a terminal
+2- Run `yarn hardhat node`
+
+3- Open another terminal
+4- run `yarn hardhat run scripts/{script_name}.ts --network localhost`
---
+++ scripts/mint-and-list-item.ts
@@ -0,0 +1,33 @@
+const { ethers, network } = require("hardhat");
+const { moveBlocks } = require("../utils/move-blocks");
+import { NftMarketplace, BasicNft } from "../typechain-types";
+
+const PRICE = ethers.utils.parseEther("0.1");
+const sleepAmount: number = 1000;
+
+async function mintAndList() {
+  const nftMarketplace: NftMarketplace = await ethers.getContract("NftMarketplace");
+  const basicNft: BasicNft = await ethers.getContract("BasicNft");
+
+  console.log("Minting NFT...");
+  const mintTx = await basicNft.mintNft();
+  const mintTxReceipt = await mintTx.wait(1);
+  const tokenId = mintTxReceipt.events[0].args.tokenId;
+  console.log("Approving NFT...");
+  const approvalTx = await basicNft.approve(nftMarketplace.address, tokenId);
+  await approvalTx.wait(1);
+  console.log("Listing NFT...");
+  const tx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE);
+  await tx.wait(1);
+  console.log("NFT Listed!");
+  if (network.config.chainId == 31337) {
+    await moveBlocks(1, sleepAmount);
+  }
+}
+
+mintAndList()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
