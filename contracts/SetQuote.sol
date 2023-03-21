696d8f08c45252d551a558f99e2a3771f069a792
UrbanWill <willian.urban@gmail.com>
2023-03-21 09:42:34 +0700

    added notice comment to temp contract

--- contracts/SetQuote.sol
+++ contracts/SetQuote.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.7;
 
 import { EIP712MetaTransaction } from "./base/EIP712/EIP712MetaTransaction.sol";
 
+/*
+ * @notice This is a temporary contract to test the EIP712MetaTransaction contract
+ */
 contract SetQuote is EIP712MetaTransaction("TestContract", "1") {
     string public quote;
     address public owner;
